5763117379dc697c897736842ee5a3c467824a76
salvadouur <salvadorsierranqn@gmail.com>
2023-10-27 23:56:11 -0300

    se definen las imagenes

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,11 @@ function init() {
         game.start();
 }
 
+/* se crean objetos que van a contener las imagenes para que se generen una sola vez */
+var imageRepository = new function() {
+    this.fondo = new Image();
+    this.nave =  new Image();
+    this.enemigo = new Image();
+    this.balaEnemigo = new Image();
+    this.bala = new Image();
+}
