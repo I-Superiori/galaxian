c34bb1ba22abdfec605add0e97d53fe6a7e6c148
smurenu <murenu.santiago.epet20@gmail.com>
2023-11-13 00:23:45 -0300

    le agrego una funcion para que cada vez que toque el sable laser me mande a la entrada correspondiente, sea de los buenos o los malos

---
+++ JS/eleccion.js
@@ -0,0 +1,11 @@
+document.getElementById('vaderSable').addEventListener('click', function() {
+    // Acción para el sable de Darth Vader
+
+    window.location.href = 'entradaImperio.html';
+});
+
+document.getElementById('obiWanSable').addEventListener('click', function() {
+ 
+    // Redirige a la página entrada.html
+    window.location.href = 'entrada.html';
+});
