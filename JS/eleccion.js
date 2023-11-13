cca10b8bd741f07a0214e0f77feb3b632602a3fc
smurenu <murenu.santiago.epet20@gmail.com>
2023-11-13 10:41:35 -0300

    creo el index para la version del imperio, esto va a servir cuando hagamos el onclick eligiendo oscuro

--- JS/eleccion.js
+++ JS/eleccion.js
@@ -9,3 +9,8 @@ document.getElementById('obiWanSable').addEventListener('click', function() {
     // Redirige a la página entrada.html
     window.location.href = 'entrada.html';
 });
+function playSound(soundId) {
+    var sound = document.getElementById(soundId);
+    sound.currentTime = 2;  // Reinicia el sonido si ya está reproduciéndose
+    sound.play();
+}
